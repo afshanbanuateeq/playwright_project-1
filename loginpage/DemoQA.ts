0e610dcadaf3cc9a2d961d640cd766ace60837ed
Laxman <laxmankosgi54@gmail.com>
2025-10-15 15:40:03 +0530

    handle multiple pages

--- loginpage/DemoQA.ts
+++ loginpage/DemoQA.ts
@@ -1,25 +1,25 @@
-import { Locator, Page } from '@playwright/test';
+import { Page, expect } from '@playwright/test';
 
-class DemoQALoginPage{
-    page:Page;
+class DemoQALoginPage {
+  readonly page: Page;
 
-    constructor(p:Page){
-        this.page = p;
-    }
+  constructor(page: Page) {
+    this.page = page;
+  }
 
-    async loginToDemoQA(username:string,password:string){
-  await this.page.goto("https://demoqa.com");
-    await this.page.getByText("Book Store Application", { exact: true }).click();
-    await this.page.locator("#login").click();
+  async loginToDemoQA(username: string, password: string) {
+    await this.page.goto('https://demoqa.com');
+    await this.page.getByText('Book Store Application', { exact: true }).click();
+    await this.page.locator('#login').click();
     await this.page.locator('#userName').fill(username);
-     await this.page.locator('#password').fill(password);
-     await this.page.locator("#login").click();
+    await this.page.locator('#password').fill(password);
+    await this.page.locator('#login').click();
+  }
 
-    }
-
-   async verifyPlaceHolder(placeHolderName:string){
-      await this.page.getByPlaceholder(placeHolderName)
-    }
+  async verifyPlaceHolder(placeHolderName: string) {
+    const input = this.page.getByPlaceholder(placeHolderName);
+    await expect(input).toBeVisible(); // ✅ use assertion to verify
+  }
 }
 
 export default DemoQALoginPage;
